c93207439c82e9ab7899f3ecc19333d2138a488f
Cookie monster <emailemail.com>
2017-05-10 16:41:42 -0500

    Figuring it out.

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -12,11 +12,25 @@ import java.io.FileReader;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    protected String type;
+    protected String urlMain;
+    //protected typeUrl;
 
-    public static void main(String[] args) throws Exception{
-	// write your code here
+    public Main(String main) {
+    }
+
+    public Main() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner inputScanner = new Scanner(System.in);
+
+        System.out.println("Please enter Super Hero you are looking for: ");
+        String searchHero = inputScanner.nextLine();
 
         BufferedReader reader = new BufferedReader(new FileReader("keys.txt"));
 
@@ -39,42 +53,42 @@ public class Main {
 
         // Now have ts and hash, and can make API call.
 
-        String baseurl = "https://gateway.marvel.com/v1/public/characters?ts=%s&apikey=%s&hash=%s";
+        String baseurl = "https://gateway.marvel.com:443/v1/public/characters?name=" +searchHero + "&ts=%s&apikey=%s&hash=%s";
         String url = String.format(baseurl, ts, publicKey, hash);
 
         System.out.println(url);
 
         AsyncHttpClient c = new DefaultAsyncHttpClient();
-        c.prepareGet(url).execute(new AsyncCompletionHandler<Response>() {
+        c.prepareGet(url).execute(new AsyncCompletionHandler<String>() {
             @Override
-            public Response onCompleted(Response response) throws Exception {
+            public String onCompleted(Response response) throws Exception {
 
-try {
-                JSONObject jsonResponse = new JSONObject(response.getResponseBody());
-                JSONArray resultArray = jsonResponse.getJSONObject("data").getJSONArray("results");
-               // System.out.println(resultObject);
+                try {
+                    JSONObject jsonResponse = new JSONObject(response.getResponseBody());
+                    JSONArray resultArray = jsonResponse.getJSONObject("data").getJSONArray("results");
+                    //System.out.println(jsonResponse);
 
-                for (int x = 0 ; x < resultArray.length() ;  x++) {
+                    // System.out.println(resultObject);
+
+                    for (int x = 0; x < resultArray.length(); x++) {
 //
-                    JSONObject character = resultArray.getJSONObject(x);
-                    System.out.println(character);
-                    System.out.println(character.getString("name"));
+                        JSONObject character = resultArray.getJSONObject(x);
+                        //System.out.println(character);
+                        System.out.println(character.getString("name"));
+                        System.out.println(character.getString("description"));
+                        
+                    }
 
+                    return null;
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-
                 return null;
-            } catch (Exception e) {
-    e.printStackTrace();
-}return null;
             }
         });
 
-
-
-
-
-
-
-
     }
+
+
+    
 }
